c730da1e57f2c726d3bdf70523b42cfd8592a4ba
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-06-09 23:22:33 +0800

    Create [[path]].js

---
+++ functions/api/[[path]].js
@@ -0,0 +1,29 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url)
+
+  const targetHost = 'https://xv.18xnxx.cfd'
+
+  const targetUrl =
+    targetHost +
+    url.pathname.replace(/^\/api/, '') +
+    url.search
+
+  const init = {
+    method: context.request.method,
+    headers: context.request.headers
+  }
+
+  if (
+    context.request.method !== 'GET' &&
+    context.request.method !== 'HEAD'
+  ) {
+    init.body = await context.request.text()
+  }
+
+  const response = await fetch(targetUrl, init)
+
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers
+  })
+}
